f79f0dbd645181180f67ebb4ba598d6b54f36008
unDocUMeantIt <m@reaktanz.de>
2017-05-05 17:36:48 +0200

    initial commit

---
+++ R/sylly.it-package.R
@@ -0,0 +1,27 @@
+#' Language Support for 'sylly' Package: Italian.
+#'
+#' \tabular{ll}{
+#' Package: \tab sylly.it\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1-1\cr
+#' Date: \tab 2017-05-05\cr
+#' Depends: \tab R (>= 3.1),sylly\cr
+#' Encoding: \tab UTF-8\cr
+#' License: \tab GPL (>= 3)\cr
+#' LazyLoad: \tab yes\cr
+#' URL: \tab http://reaktanz.de/?c=hacking&s=koRpus\cr
+#' }
+#'
+#' Adds support for the Italian language to the 'sylly' package. Due to some restrictions on
+#' CRAN, the full package sources are only available from the project homepage. To ask for
+#' help, report bugs, suggest feature improvements, or discuss the global development of the
+#' package, please consider subscribing to the koRpus-dev mailing list
+#' (<http://korpusml.reaktanz.de>).
+#'
+#' @aliases sylly.it-package
+#' @name sylly.it-package
+#' @docType package
+#' @title The sylly.it Package
+#' @author Meik Michalke
+#' @keywords package
+NULL
